630851e505eb4de6848eb0c9295acf0754d39565
alan9130314 <alan9130314@gmail.com>
2026-05-08 16:29:45 +0800

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let cars = position.map((pos, idx) => [pos, speed[idx]]);
+        let sortCars = cars.sort((a, b) => b[0] - a[0]);
+        let stack = [];
+        console.log(cars, "|", sortCars);
+        for (const car of sortCars) {
+            let time = (target - car[0]) / car[1];
+            stack.push(time);
+            console.log(
+                stack[stack.length - 1],
+                stack[stack.length - 2],
+                stack[stack.length - 1] <= stack[stack.length - 2],
+            );
+            if (stack.length >= 2 && stack[stack.length - 1] <= stack[stack.length - 2]) {
+                stack.pop();
+            }
+        }
+        return stack.length;
+    }
+}
