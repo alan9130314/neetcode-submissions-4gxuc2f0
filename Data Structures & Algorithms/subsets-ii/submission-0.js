08d41781e720e0856482c8a53edb244c7bdc125d
Alan Wang <alan9130314@gmail.com>
2026-07-23 09:42:55 +0800

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsetsWithDup(nums) {
+        nums.sort((a, b) => a - b);
+        const res = [];
+        const path = [];
+        function backtrack(start) {
+            res.push([...path]);
+
+            for (let i = start; i < nums.length; i++) {
+                if (i > start && nums[i] === nums[i - 1]) {
+                    continue;
+                }
+
+                path.push(nums[i]);
+                backtrack(i + 1);
+                path.pop();
+            }
+        }
+        backtrack(0);
+        return res;
+    }
+}
